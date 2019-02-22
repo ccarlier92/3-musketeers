4b3ec401634a9450d54f019eeeae58452643b6f4
Céline <celine.carlier@devinci.fr>
2019-02-22 12:12:32 +0100

    all starwars tests passed

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -3,29 +3,43 @@ const starWarsNames = require('./starwars-names.json');
 
 describe('starwars-names', () => {
   describe('all', () => {
-    test('should be a fulfilled array', () => {
-      expect(starWars.all).toHaveLength(starWarsNames.length)
-    });
-    test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
+  test('should be a fulfilled array', () => {
+    expect(starWars.all).toHaveLength(starWarsNames.length)
+});
+  test('should be an array of strings', () => {
+    for( var i = 0; i < starWarsNames.length; i++)
+  {
+    expect(typeof(starWarsNames[i])).toBe("string");
+  }
 
-    test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
+});
 
-    test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
-  });
+  test('should contain `Luke Skywalker`', () => {
 
-  describe('random', () => {
-    test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
 
-    test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
-    });
-  });
+  expect(starWarsNames).toEqual(expect.arrayContaining(["Luke Skywalker"]));
+
+});
+
+  test('should not contain `Ben Quadinaros`', () => {
+
+
+  expect(starWars.all).not.toContain("Ben Quadinaros");
+
+});
+});
+
+describe('random', () => {
+  test('should return a random item from the starWars.all', () => {
+  expect(starWars.all).toContain(starWars.random());
+
+
+});
+
+test('should return an array of random items if passed a number', () => {
+  var i = Math.floor((Math.random() * starWars.length)+1);
+expect(starWars.all).toEqual(expect.arrayContaining(starWars.random(i)));
+
+});
+});
 });
